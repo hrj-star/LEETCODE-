274e09dd4aae48f57b9649211c3de3512670bfab
hrj-star <hetjoshi022@gmail.com>
2020-12-10 23:33:53 +0530

    Majority Element

---
+++ 169. Majority Element/Solution.java
@@ -0,0 +1,43 @@
+// Brute Force Approach
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int n=nums.length;
+        int max_count=n/2;
+    
+        for(int i=0;i<n;i++){
+            int count=0;
+            
+            for(int j=0;j<n;j++){
+                
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }     // inner for close 
+            
+            if(count > max_count)
+                return nums[i];
+ 
+        } // outer for close
+        
+        return -1;
+    
+    }
+}
+
+
+
+
+/* 
+
+Complexity Analysis :
+
+TC : O(n^2)  - The brute force algorithm contains two nested for loops that each run for n iterations, adding up to quadratic time complexity.
+
+SC : O(1) - constant space 
+
+*/
+
+*/
+
