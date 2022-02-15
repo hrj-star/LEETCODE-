b7eb6ce2e7ac0c240ad416e94613d2ade2113103
hrj-star <hetjoshi022@gmail.com>
2022-02-15 10:03:51 +0530

    Feb DCC Day 15

--- 136. Single Number/Solution.java
+++ 136. Single Number/Solution.java
@@ -1,5 +1,5 @@
 
-// HashSet 
+// using HashSet - iterate elements , if duplicate found then keep removing from set , at last , set will contain single unique number -- 2 , 2 , 1 => set => [2] => [] => [1] , return 1
 
 class Solution {
     public int singleNumber(int[] nums) {
@@ -21,8 +21,14 @@ class Solution {
 /*
 
 Complexity analysis :
-
+----------------------------
 TC : O(N) - O(N) for traversing in for loop , O(1) for HashSet operations -> O(N.1) = O(N)
 SC : O(N) - space by hashset to hold array elements.
+
+
+Verdict :
+-----------------------------
+Runtime: 19 ms, faster than 18.72% of Java online submissions for Single Number.
+Memory Usage: 54.2 MB, less than 5.21% of Java online submissions for Single Number.
 
 */
